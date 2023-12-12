023060ca3039d02dae6dbd71604539f439c39731
Matthew Brooks <mb964033@gmail.com>
2023-12-12 21:06:23 +0000

    update santoku

--- config.lua
+++ config.lua
@@ -1,18 +1,19 @@
 local env = {
 
   name = "santoku-system",
-  version = "0.0.1-1",
+  version = "0.0.2-1",
   variable_prefix = "TK_SYSTEM",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.143-1",
+    "santoku >= 0.0.145-1",
     "luaposix >= 36.2.1-1"
   },
 
   test_dependencies = {
+    "santoku-test >= 0.0.1-1",
     "luassert >= 1.9.0-1",
     "luacheck >= 1.1.0-1",
     "luacov >= 0.15.0-1",
