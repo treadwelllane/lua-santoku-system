6db44426fddeef302d45cda5b1f06c60648d76a9
Matthew Brooks <mb964033@gmail.com>
2024-02-07 01:03:33 +0000

    update libs

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
 
   name = "santoku-system",
-  version = "0.0.16-1",
+  version = "0.0.17-1",
   variable_prefix = "TK_SYSTEM",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.178-1",
+    "santoku >= 0.0.179-1",
   },
 
   test = {
