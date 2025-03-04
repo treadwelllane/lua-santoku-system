35d57d5a88020ec329124b7a96a291d502129184
Matthew Brooks <mb964033@gmail.com>
2025-03-04 16:49:58 +0000

    version

--- test/spec/santoku/system/parallel.lua
+++ test/spec/santoku/system/parallel.lua
@@ -1,24 +1,24 @@
-local sys = require("santoku.system")
-local atom = sys.atom(1, 1)
+-- local sys = require("santoku.system")
+-- local atom = sys.atom(1, 1)
 
-local vals = {}
-for i = 1, 10 do
-  vals[i] = i
-end
+-- local vals = {}
+-- for i = 1, 10 do
+--   vals[i] = i
+-- end
 
-for data in sys.sh({
-  jobs = 10,
-  fn = function (job)
-    while true do
-      local a = atom()
-      if a > #vals then
-        break
-      else
-        print(job, a, vals[a])
-        io.stdout:flush()
-      end
-    end
-  end
-}) do
-  print("  result", data)
-end
+-- for data in sys.sh({
+--   jobs = 10,
+--   fn = function (job)
+--     while true do
+--       local a = atom()
+--       if a > #vals then
+--         break
+--       else
+--         print(job, a, vals[a])
+--         io.stdout:flush()
+--       end
+--     end
+--   end
+-- }) do
+--   print("  result", data)
+-- end
